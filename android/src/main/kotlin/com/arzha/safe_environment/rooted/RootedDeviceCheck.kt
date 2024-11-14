382fee252be2515b608fb7776d9b60ff55e8cd1e
Arianto Zaki Hamdalah <arianto.zaki.hamdalah.03@gmail.com>
2024-11-14 15:09:12 +0700

    Update RootedDeviceCheck.kt

--- android/src/main/kotlin/com/arzha/safe_environment/rooted/RootedDeviceCheck.kt
+++ android/src/main/kotlin/com/arzha/safe_environment/rooted/RootedDeviceCheck.kt
@@ -3,21 +3,22 @@ package com.arzha.safe_environment.rooted
 import android.content.Context
 import android.os.Build
 import com.scottyab.rootbeer.RootBeer
+import java.io.File
 import java.util.*
 
-
 class RootedDeviceCheck {
-    private static final String[] rootsAppPackage = {
+    companion object {
+        private val rootsAppPackage = arrayOf(
             "com.noshufou.android.su",
             "com.noshufou.android.su.elite",
             "eu.chainfire.supersu",
             "com.koushikdutta.superuser",
             "com.thirdparty.superuser",
             "com.yellowes.su",
-            "com.topjohnwu.magisk",
-    };
+            "com.topjohnwu.magisk"
+        )
 
-    public static final String[] dangerousListApps ={
+        private val dangerousListApps = arrayOf(
             "com.koushikdutta.rommanager",
             "com.koushikdutta.rommanager.license",
             "com.dimonvideo.luckypatcher",
@@ -26,9 +27,9 @@ class RootedDeviceCheck {
             "com.ramdroid.appquarantinepro",
             "com.android.vending.billing.InAppBillingService.COIN",
             "com.chelpus.luckypatcher"
-    };
+        )
 
-    private static final String[] rootCloakingApps ={
+        private val rootCloakingApps = arrayOf(
             "com.devadvance.rootcloak",
             "com.devadvance.rootcloakplus",
             "de.robv.android.xposed.installer",
@@ -37,11 +38,10 @@ class RootedDeviceCheck {
             "com.amphoras.hidemyroot",
             "com.amphoras.hidemyrootadfree",
             "com.formyhm.hiderootPremium",
-            "com.formyhm.hideroot",
+            "com.formyhm.hideroot"
+        )
 
-    };
-
-    private static final String[] superUserPath = {
+        private val superUserPath = arrayOf(
             "/data/local/su",
             "/data/local/bin/su",
             "/data/local/xbin/su",
@@ -63,38 +63,35 @@ class RootedDeviceCheck {
             "/system/xbin/daemonsu",
             "/data/adb/magisk.img",
             "/data/adb/magisk",
-            "/data/adb/.magisk",
-    };
+            "/data/adb/.magisk"
+        )
 
-
-    private static final String[] notWritablePath ={
+        private val notWritablePath = arrayOf(
             "/system",
             "/system/bin",
             "/system/sbin",
             "/system/xbin",
             "/vendor/bin",
             "/sbin",
-            "/etc",
-    };
+            "/etc"
+        )
 
-    private static boolean checkFiles(List<String> targets){
-        for(String pipe : targets){
-            File file = new File(pipe);
-            if (file.exists())
-                return true;
+        private fun checkFiles(targets: Array<String>): Boolean {
+            for (path in targets) {
+                val file = File(path)
+                if (file.exists()) return true
+            }
+            return false
         }
-        return false;
-    }
 
-    private static boolean checkRootFiles (){
-        return (checkFiles(rootsAppPackage)
-                || checkFiles(dangerousListApps)
-                || checkFiles(rootCloakingApps)
-                || checkFiles(superUserPath)
-                || checkFiles(notWritablePath));
-    }
+        private fun checkRootFiles(): Boolean {
+            return (checkFiles(rootsAppPackage)
+                    || checkFiles(dangerousListApps)
+                    || checkFiles(rootCloakingApps)
+                    || checkFiles(superUserPath)
+                    || checkFiles(notWritablePath))
+        }
 
-    companion object {
         private const val ONEPLUS = "oneplus"
         private const val MOTO = "moto"
         private const val XIAOMI = "xiaomi"
@@ -116,7 +113,7 @@ class RootedDeviceCheck {
                 rootBeer.isRootedWithBusyBoxCheck
             } else {
                 rootBeer.isRooted
-            }  
+            }
         }
     }
 }
